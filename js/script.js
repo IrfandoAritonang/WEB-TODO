2faf7eba3ce2aeed04c679a8ac5810344a1b530f
Irfando <irfandoaritonang86@gmail.com>
2022-08-28 23:42:31 +0700

    Done

--- js/script.js
+++ js/script.js
@@ -11,22 +11,118 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
 }
 
 function generateId() {
-    return +new Date();
-  }
+  return +new Date();
+}
 
 //fungsi add todo
 function addTodo() {
-    const textTodo = document.getElementById("title").value;
-    const timestamp = document.getElementById("date").value;
-  
-    const generatedID = generateId();
-    const todoObject = generateTodoObject(generatedID,textTodo,timestamp,false);
-    todos.push(todoObject);
-  
-    document.dispatchEvent(new Event(RENDER_EVENT));
-  }
-  
+  const textTodo = document.getElementById("title").value;
+  const timestamp = document.getElementById("date").value;
 
+  const generatedID = generateId();
+  const todoObject = generateTodoObject(
+    generatedID,
+    textTodo,
+    timestamp,
+    false
+  );
+  todos.push(todoObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+//fungsi make Todo
+function makeTodo(todoObject) {
+  const textTitle = document.createElement("h2");
+  textTitle.innerText = todoObject.task;
+
+  const textTimestamp = document.createElement("p");
+  textTimestamp.innerText = todoObject.timestamp;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("inner");
+  textContainer.append(textTitle, textTimestamp);
+
+  const container = document.createElement("div");
+  container.classList.add("item", "shadow");
+  container.append(textContainer);
+  container.setAttribute("id", `todo-${todoObject.id}`);
+
+  if (todoObject.isCompleted) {
+    const undoButton = document.createElement("button");
+    undoButton.classList.add("undo-button");
+
+    undoButton.addEventListener("click", function () {
+      undoTaskFromCompleted(todoObject.id);
+    });
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-button");
+
+    trashButton.addEventListener("click", function () {
+      removeTaskFromCompleted(todoObject.id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+
+    checkButton.addEventListener("click", function () {
+      addTaskToCompleted(todoObject.id);
+    });
+
+    container.append(checkButton);
+  }
+
+  return container;
+}
+function findTodo(todoId) {
+  for (const todoItem of todos) {
+    if (todoItem.id === todoId) {
+      return todoItem;
+    }
+  }
+  return null;
+}
+
+function removeTaskFromCompleted(todoId) {
+  const todoTarget = findTodoIndex(todoId);
+
+  if (todoTarget === -1) return;
+
+  todos.splice(todoTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoTaskFromCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function addTaskToCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+//hapus data
+function findTodoIndex(todoId) {
+    for (const index in todos) {
+      if (todos[index].id === todoId) {
+        return index;
+      }
+    }
+  
+    return -1;
+  }
 
 document.addEventListener("DOMContentLoaded", function () {
   const submitForm = document.getElementById("form");
@@ -36,11 +132,32 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+//hapus data
+function findTodoIndex(todoId) {
+  for (const index in todos) {
+    if (todos[index].id === todoId) {
+      return index;
+    }
+  }
 
-
-
-//
+  return -1;
+}
 
 document.addEventListener(RENDER_EVENT, function () {
-  console.log(todos);
+  //untuk menampilkan data todo yang harus dilakukan
+  const uncompletedTODOList = document.getElementById("todos");
+  uncompletedTODOList.innerHTML = "";
+
+  //untuk menampilkan data todo yang sudah dilakukan
+  const completedTODOList = document.getElementById("completed-todos");
+  completedTODOList.innerHTML = "";
+
+  for (const todoItem of todos) {
+    const todoElement = makeTodo(todoItem);
+    if (!todoItem.isCompleted) {
+      uncompletedTODOList.append(todoElement);
+    } else {
+      completedTODOList.append(todoElement);
+    }
+  }
 });
